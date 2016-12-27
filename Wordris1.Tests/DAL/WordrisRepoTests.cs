bb845eea1c6c21e115ba689fc98432da8cb88a0f
Scott Schaffer <scottpschaffer@hotmail.com>
2016-12-27 13:46:39 -0800

    Adding Moq Tests

---
+++ Wordris1.Tests/DAL/WordrisRepoTests.cs
@@ -0,0 +1,39 @@
+﻿using Moq;
+using System;
+using System.Data.Entity;
+using Wordris1.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Wordris1.DAL;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Wordris1.Tests.DAL
+{
+    [TestClass]
+    public class WordrisRepoTests
+    {
+        private Mock<DbSet<Word>> mock_words { get; set; }
+        private Mock<DbSet<Score>> mock_scores { get; set; }
+        private Mock<WordrisContext> mock_context { get; set; }
+        public MockRepository Repo { get; set; }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+
+        }
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            mock_context = new Mock<WordrisContext>();
+            mock_words = new Mock<DbSet<Word>>();
+            mock_scores = new Mock<DbSet<Score>>();
+            Repo = new MockRepository(mock_context.Object);
+
+
+        }
+    }
+}
